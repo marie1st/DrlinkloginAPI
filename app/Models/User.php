9b4c2fa426062e3b29c0cf91ac02c78de3f600db
Mariebouquin <marie1st7880@gmail.com>
2021-01-24 17:15:48 +0700

    fifth commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,7 +29,6 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
     /**
@@ -38,6 +37,6 @@ class User extends Authenticatable
      * @var array
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
+        
     ];
 }
